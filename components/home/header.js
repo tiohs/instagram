964a03f3cf57089663201cc1296684395b65223a
tiohs <hamiltonsilva551@gmail.com>
2022-02-01 09:40:06 +0100

    require image

--- components/home/header.js
+++ components/home/header.js
@@ -1,12 +1,27 @@
-import { View, Text } from 'react-native';
+import { View, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import React from 'react';
 
 const header = () => {
   return (
-    <View>
-      <Text style = {{ color : 'white'}}>Header</Text>
-    </View>
+    <TouchableOpacity>
+      <View>
+        <Image style={styles.logo} source={require('../../assets/header-logo.png')} />
+      </View>
+    </TouchableOpacity>
+    
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+
+  },
+  logo : {
+    width: 100,
+    height: 50,
+    resizeMode: 'contain',
+  }
+})
+
 export default header;
+
