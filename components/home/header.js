5141dacc757b87232f17bcaaded0ee8fd4875ab4
tiohs <hamiltonsilva551@gmail.com>
2022-02-01 09:57:30 +0100

    header update

--- components/home/header.js
+++ components/home/header.js
@@ -1,25 +1,49 @@
 import { View, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+
 import React from 'react';
 
 const header = () => {
   return (
-    <TouchableOpacity>
-      <View>
+      <View style={styles.container}>
+         <TouchableOpacity>
         <Image style={styles.logo} source={require('../../assets/header-logo.png')} />
+        </TouchableOpacity>
+        <View style={styles.iconContainer}>
+          <TouchableOpacity>
+            <Image style={styles.icon} source={require('../../assets/plus-2-math.png')}/>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image style={styles.icon} source={require('../../assets/like--v1.png')}/>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image style={styles.icon} source={require('../../assets/facebook-messenger.png')}/>
+          </TouchableOpacity>
+        </View>
       </View>
-    </TouchableOpacity>
-    
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginHorizontal: 20
   },
   logo : {
     width: 100,
     height: 50,
     resizeMode: 'contain',
+  },
+  iconContainer : {
+    flexDirection: 'row',
+  },
+  icon: {
+    width: 30,
+    height: 30,
+    marginLeft: 10,
+    resizeMode: 'contain'
   }
 })
 
